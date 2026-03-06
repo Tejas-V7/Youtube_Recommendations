a39e7ff340047f4bec07695fc24a693a1aae79ee
Tejas V <tejupro007@gmail.com>
2026-03-06 16:07:19 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [video, setVideo] = useState([]);
 
  const getVideo = async () => {
-  const res = await axios.post("http://localhost:5000/mood", {
+  const res = await axios.post("https://youtube-recommendations-j6bq.onrender.com/mood", {
     text: text
   });
 
